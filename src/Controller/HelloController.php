0f4106bd1d4d1cf59b42312e9c21a7ad784b76c8
Diego Ply <ply.dgo@gmail.com>
2024-07-17 11:53:03 +0200

    Contact & CGU added

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HelloController extends AbstractController
 {
-    #[Route('/', name: 'app_hello')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('hello/index.html.twig', [
             'controller_name' => 'HelloController',
+            'title' => 'Home',
         ]);
     }
 }
