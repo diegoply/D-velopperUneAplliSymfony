c1d3c8bab2680bc2822701bd8dea1846f5d4dbae
Diego Ply <ply.dgo@gmail.com>
2024-07-18 19:11:40 +0200

    formulaire en cours

--- src/Controller/MarqueController.php
+++ src/Controller/MarqueController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Marque;
+use App\Form\MarqueType;
 use App\Repository\MarqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,9 +30,21 @@ class MarqueController extends AbstractController
     }
 
     #[Route('/marque/create', name: 'marque_create', priority: 10, methods: ['GET', 'POST'])]
-    public function create(): Response
+    public function create(Request $request, MarqueRepository $repo): Response
     {
-        dd(__METHOD__);
+
+        $marque = new Marque();
+
+        $form = $this->createForm(MarqueType::class, $marque);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $repo->save($marque, true);
+            
+        }
+
+        return $this->render('marque/create.html.twig',[
+            'formView' => $form->createView(),
+        ]);
     }
 
     #[Route('/marque/{id}/edit', name: 'marque_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
